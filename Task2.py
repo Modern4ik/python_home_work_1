94078404ce312bdddeadc6c8c2f9366daefa1706
Sergey Zaytsev <surgery_333@mail.ru>
2023-06-16 17:27:52 +0300

    Task2 solved and file Task2.py with solution modified

---
+++ Task2.py
@@ -0,0 +1,22 @@
+# Программа для поиска суммы цифр трёхзначного числа.
+
+user_number = 0
+sum_digit = 0
+flag = True
+
+while flag:
+
+    user_number = int(input("Введите трёхзначное число: "))
+    
+    if user_number > 99 and user_number < 1000:
+        flag = False
+    else:
+        print("Ошибка ввода!Повторите попытку.")
+
+while user_number / 10 > 0:
+
+    sum_digit += user_number % 10
+
+    user_number = user_number // 10
+
+print("Сумма цифр в данном трёзначном числе равняется ->", sum_digit)
